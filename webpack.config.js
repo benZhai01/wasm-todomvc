3a66ada66ad1fdf6087e80be7da9b95a98f4e276
benzhai <ben.zhai@grapecity.com>
2020-11-06 14:11:59 +0800

    add icon.

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,45 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, "."),
-            args: '--log-level verbose'
-        }),
-        // Have this example work in Edge which doesn't ship `TextEncoder` or
-        // `TextDecoder` at this time.
-        new webpack.ProvidePlugin({
-          TextDecoder: ['text-encoding', 'TextDecoder'],
-          TextEncoder: ['text-encoding', 'TextEncoder']
-        })
-    ],
-    mode: 'development',
-    devtool: 'source-map'
-};
+module.exports = env => {
+    
+    let config = {
+        entry: './src/js/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            }),
+            new WasmPackPlugin({
+                crateDirectory: path.resolve(__dirname, "."),
+                args: '--log-level verbose'
+            }),
+            // Have this example work in Edge which doesn't ship `TextEncoder` or
+            // `TextDecoder` at this time.
+            new webpack.ProvidePlugin({
+            TextDecoder: ['text-encoding', 'TextDecoder'],
+            TextEncoder: ['text-encoding', 'TextEncoder']
+            })
+        ],
+        mode: 'development',
+        devtool: 'source-map'
+    };
+
+    if(env == 'prod'){
+        config.mode = 'production';
+        config.devtool = 'none';
+        config.output.path = path.resolve(__dirname, 'artifacts');
+        config.plugins.push(new CopyPlugin({
+            patterns: [
+                {from: 'asset', to: 'asset'}
+            ]
+        }))
+    }
+
+    return config;
+}
